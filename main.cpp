90c2cbc49e42370d0bb95e4cbc6bbe44e6b9634e
Ali Saeed <asaeed5637@gmail.com>
2024-10-25 18:06:52 -0700

    #7 (final)  commit

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+// comsc-210 | Ali Saeed \ Lab 23
+// ide used; vs code
+
 #include <iostream>
 #include <fstream>
 #include <iomanip>
@@ -7,6 +10,7 @@ using namespace std;
 
 const int SZ_NAMES = 200, SZ_COLORS = 25, MAX_AGE = 20;
 
+//function prototypes
 int select_goat(list<Goat> trip);
 void delete_goat(list<Goat> &trip);
 void add_goat(list<Goat> &trip, string [], string []);
@@ -28,9 +32,12 @@ int main() {
     while (fin1 >> colors[i++]);
     fin1.close();
 
+    //list
     list<Goat> trip;
 
+    //loop that runs the main_menu() function until quit is chosen
     int choice = 0;
+    cout << "\t*** GOAT MANAGER 3001 ***\n";
     while (choice != 4) {
         choice = main_menu();
         cout << endl;
@@ -53,16 +60,19 @@ int main() {
         else {
             cout << "Invalid choice \n";
         }
-        cout << endl << endl;
+        cout << endl;
     }
     //first run attempt resulted in lots of errors with the auto type specifier have to update my coderuner
     //actually can ust try other thing to run it
+    //running now thru extension modifications but it doesnt delete (maybe my conditions i put into one of the functions)
+    //working correctly!
+    //still getting the warnings with the auto type C++11 extension but it should be because of my c++ version
 
     return 0;
 }
 
+//main menu function output and takes in the choice
 int main_menu() {
-    cout << "\t*** GOAT MANAGER 3001 ***\n";
     cout << "\t[1] Add a goat\n";
     cout << "\t[2] Delete a goat\n";
     cout << "\t[3] List goats\n";
@@ -85,6 +95,7 @@ void add_goat(list<Goat> &trip, string names[], string colors[]) {
     trip.push_back(addedGoat);
     cout << "\tGoat added, Name: " << name << ", Age: " << age << ", Color: " << color << "\n";
 }
+
 //display the trip so that we can see all elements in list for deleting (use select with this and then use delete with the select)
 void display_trip(list<Goat> trip) {
     if (trip.empty()) {
@@ -92,6 +103,7 @@ void display_trip(list<Goat> trip) {
         return;
     }
     int i = 1;
+    //looked into video notes for tjhis function
     for (auto it = trip.begin(); it!= trip.end(); ++it) {
         cout << "\t\t[" << i << "] " << it->get_name() << " (" << it->get_age() << ", " << it->get_color() << ")\n";
         i++;
@@ -100,31 +112,38 @@ void display_trip(list<Goat> trip) {
 
 //select number of goat for now to be used to delete
 int select_goat(list<Goat> trip) {
+    //make sure it has at east one element
     if (trip.empty()) {
         cout << "List empty.";
         return -1;
     }
+    //access display function to let you choose
     display_trip(trip);
 
     int choice;
-    cout << "Enter the number of the goat you want to perform this action on: ";
+    //get user input for choice
+    cout << "\tEnter the number of the goat you want to perform this action on: ";
     cin >> choice;
 
-    return choice; 
+    return choice - 1; 
 }
 
+//make sure its valid then go iterate and delete it
 void delete_goat(list<Goat> &trip) {
     auto it = trip.begin();
-    int choice = select_goat(trip) - 1;
+    int choice = select_goat(trip);
 
-    if (choice < 1 || choice > trip.size()) {
+    //make sure selection not out of bounds
+    if (choice < 0 || choice > trip.size()) {
         cout << "Invalid sleection";
         return;
     }
 
+    //go to the specific element in list
     for (int i = 0; i < choice; i++) {
         it++;
     }
+    //erase it at that position
     trip.erase(it);
 }
 
